e8eb3b4b9f18638a63d9ff434819c5d29fd42295
jackie <jackieliu_cornell@163.com>
2021-10-19 13:59:03 +0800

    update

--- finetune/generation/run_gen_v2.py
+++ finetune/generation/run_gen_v2.py
@@ -6,79 +6,82 @@ import random
 import sys
 import nltk
 
-
 import numpy as np
+from datasets import load_dataset
 import torch
 import transformers
-from transformers import (AutoConfig, AutoModel, BertTokenizer,BertForTokenClassification,
-                          DataCollatorForTokenClassification, HfArgumentParser,DataCollatorForSeq2Seq,Seq2SeqTrainer,
-                          Seq2SeqTrainingArguments, Trainer, TrainerCallback,AutoModelForSeq2SeqLM)
+from transformers import (AutoConfig, AutoModel, BertTokenizer, BertForTokenClassification,
+                          DataCollatorForTokenClassification, HfArgumentParser, DataCollatorForSeq2Seq, Seq2SeqTrainer,
+                          Seq2SeqTrainingArguments, Trainer, TrainerCallback, AutoModelForSeq2SeqLM)
 from transformers.trainer_utils import is_main_process
-from datasets import load_metric,Dataset
+from datasets import load_metric, Dataset
 from utils import DataTrainingArguments, ModelArguments, load_json
 
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from modeling_cpt import CPTModel, CPTForConditionalGeneration
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_path",default='/path/to/model',type=str)
-parser.add_argument("--dataset", default="lcsts",type=str)
-parser.add_argument("--lr",default=2e-5,type=float)
-parser.add_argument("--batch_size",default='50',type=str)
-parser.add_argument("--epoch",default='5',type=str)
-parser.add_argument("--data_dir",default="/path/to/dataset/",type=str)
+parser.add_argument("--model_path", default='/path/to/model', type=str)
+parser.add_argument("--dataset", default="lcsts", type=str)
+parser.add_argument("--lr", default=2e-5, type=float)
+parser.add_argument("--batch_size", default='50', type=str)
+parser.add_argument("--epoch", default='5', type=str)
+parser.add_argument("--data_dir", default="/path/to/dataset/", type=str)
 args = parser.parse_args()
-arg_dict=args.__dict__
+arg_dict = args.__dict__
 
 logger = logging.getLogger(__name__)
 
-dataset_name=arg_dict['dataset']
-outdir_1='output'
+dataset_name = arg_dict['dataset']
+outdir_1 = 'output'
 if not os.path.exists(outdir_1):
     os.mkdir(outdir_1)
 
-outdir=outdir_1+'/'+dataset_name
+outdir = outdir_1 + '/' + dataset_name
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
-seed=len(os.listdir(outdir))+1
-outdir=outdir+'/'+str(seed)
-length_map={'lcsts':'30','csl':'50','adgen':'128','hk01':'200'}
+seed = len(os.listdir(outdir)) + 1
+outdir = outdir + '/' + str(seed)
+length_map = {'lcsts': '30', 'csl': '50', 'adgen': '128', 'hk01': '64'}
 
-
-args=[
-    '--model_name_or_path',arg_dict['model_path'],
-    '--do_train','--do_eval','--do_predict',
-    '--train_file',os.path.join(arg_dict['data_dir'],'SUMMARY.'+dataset_name,'train.csv'),
-    '--validation_file',os.path.join(arg_dict['data_dir'],'SUMMARY.'+dataset_name,'dev.csv'),
-    '--test_file',os.path.join(arg_dict['data_dir'],'SUMMARY.'+dataset_name,'test.csv'),
-    '--output_dir',outdir,
-    '--per_device_train_batch_size',arg_dict['batch_size'],
-    '--per_device_eval_batch_size',arg_dict['batch_size'],
+args = [
+    '--model_name_or_path', arg_dict['model_path'],
+    '--do_train', '--do_eval', '--do_predict',
+    '--train_file', os.path.join(arg_dict['data_dir'], 'SUMMARY.' + dataset_name, 'train.csv'),
+    '--validation_file', os.path.join(arg_dict['data_dir'], 'SUMMARY.' + dataset_name, 'dev.csv'),
+    '--test_file', os.path.join(arg_dict['data_dir'], 'SUMMARY.' + dataset_name, 'test.csv'),
+    '--output_dir', outdir,
+    '--per_device_train_batch_size', arg_dict['batch_size'],
+    '--per_device_eval_batch_size', arg_dict['batch_size'],
     '--overwrite_output_dir',
     '--max_source_length=512',
-    '--val_max_target_length='+length_map[arg_dict['dataset']],
+    '--val_max_target_length=' + length_map[arg_dict['dataset']],
     '--predict_with_generate=1',
-    '--seed',str(1000*seed),
-    '--num_train_epochs',arg_dict['epoch'],
-    '--save_strategy','no',
-    '--evaluation_strategy','epoch',
-    '--learning_rate',str(arg_dict['lr']),
+    '--seed', str(1000 * seed),
+    '--num_train_epochs', arg_dict['epoch'],
+    '--save_strategy', 'no',
+    '--evaluation_strategy', 'epoch',
+    '--learning_rate', str(arg_dict['lr']),
+    '--max_test_samples', str(100)
 ]
 parser = HfArgumentParser((ModelArguments, DataTrainingArguments, Seq2SeqTrainingArguments))
 model_args, data_args, training_args = parser.parse_args_into_dataclasses(args)
+
+
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-        
+
+
 set_seed(training_args.seed)
 
-datasets={}
+datasets = {}
 data_files = {}
 if data_args.train_file is not None:
     data_files["train"] = data_args.train_file
@@ -86,9 +89,16 @@ if data_args.validation_file is not None:
     data_files["validation"] = data_args.validation_file
 if data_args.test_file is not None:
     data_files["test"] = data_args.test_file
-for key in data_files:
-    print(key)
-    datasets[key]=load_json(data_files[key])
+# for key in data_files:
+# print(key)
+# load csv data
+# datasets[key] = load_dataset('csv', data_files=data_files[key])
+# datasets[key]=load_json(data_files[key])
+
+# data_files = {"train": "./demo_data/SUMMARY.hk01/train.csv", "test": "./demo_data/SUMMARY.hk01/dev.csv"}
+datasets = load_dataset("csv", data_files=data_files)
+
+print ("dataset: ", datasets)
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
@@ -107,29 +117,30 @@ if is_main_process(training_args.local_rank):
     transformers.utils.logging.set_verbosity_info()
 logger.info("Training/evaluation parameters %s", training_args)
 
-tokenizer=BertTokenizer.from_pretrained(model_args.model_name_or_path)
-model=CPTForConditionalGeneration.from_pretrained(model_args.model_name_or_path)
-model.config.max_length=data_args.val_max_target_length
+tokenizer = BertTokenizer.from_pretrained(model_args.model_name_or_path)
+model = CPTForConditionalGeneration.from_pretrained(model_args.model_name_or_path)
+model.config.max_length = data_args.val_max_target_length
+model.config.max_length = data_args.max_source_length
 
-text_column='article'
-summary_column='summarization'
+text_column = 'article'
+summary_column = 'summarization'
 column_names = datasets["train"].column_names
 max_target_length = data_args.val_max_target_length
-padding=False
+padding = False
+
 
 def preprocess_function(examples):
     inputs = examples[text_column]
     targets = examples[summary_column]
     model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, padding=padding, truncation=True)
+    print ("input shape: ----", len(model_inputs['input_ids'][0]))
 
     # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
         labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
 
-
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
-
 
 
 if training_args.do_train:
@@ -176,12 +187,10 @@ if training_args.do_predict:
         load_from_cache_file=not data_args.overwrite_cache,
     )
 
-
-max_eval_num=30000
-if len(eval_dataset)>max_eval_num:
-    eval_dataset=Dataset.from_dict(eval_dataset[:max_eval_num])
+max_eval_num = 30000
+if len(eval_dataset) > max_eval_num:
+    eval_dataset = Dataset.from_dict(eval_dataset[:max_eval_num])
 print(len(eval_dataset))
-
 
 # Data collator
 label_pad_token_id = -100 if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id
@@ -192,11 +201,11 @@ data_collator = DataCollatorForSeq2Seq(
     pad_to_multiple_of=8 if training_args.fp16 else None,
 )
 
-
-
 # Metric
-from rouge import Rouge 
+from rouge import Rouge
+
 rouge = Rouge()
+
 
 def postprocess_text(preds, labels):
     preds = [pred.strip() for pred in preds]
@@ -207,10 +216,11 @@ def postprocess_text(preds, labels):
     # labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
 
     while '' in preds:
-        idx=preds.index('')
-        preds[idx]='。'
+        idx = preds.index('')
+        preds[idx] = '。'
 
     return preds, labels
+
 
 def compute_metrics(eval_preds):
     preds, labels = eval_preds
@@ -221,25 +231,27 @@ def compute_metrics(eval_preds):
         # Replace -100 in the labels as we can't decode them.
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-   
+
     # Some simple post-processing
     decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-    scores = rouge.get_scores(decoded_preds, decoded_labels,avg=True)
+    scores = rouge.get_scores(decoded_preds, decoded_labels, avg=True)
     for key in scores:
-        scores[key]=scores[key]['f']*100
+        scores[key] = scores[key]['f'] * 100
 
-    result=scores
+    result = scores
 
     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
     result["gen_len"] = np.mean(prediction_lens)
     result = {k: round(v, 4) for k, v in result.items()}
     return result
 
+
 class TestCallback(TrainerCallback):
     def on_evaluate(self, args, state, control, **kwargs):
         predictions, labels, metrics = trainer.predict(test_dataset, metric_key_prefix="predict")
-        metrics['epoch']=state.epoch
+        metrics['epoch'] = state.epoch
         state.log_history.append(metrics)
+
 
 # Initialize our Trainer
 trainer = Seq2SeqTrainer(
@@ -252,7 +264,6 @@ trainer = Seq2SeqTrainer(
     compute_metrics=compute_metrics if training_args.predict_with_generate else None,
     callbacks=[TestCallback],
 )
-
 
 # Training
 if training_args.do_train:
@@ -277,5 +288,5 @@ if trainer.is_world_process_zero():
         )
         test_preds = [pred.strip() for pred in test_preds]
         output_test_preds_file = os.path.join(training_args.output_dir, "test_generations.txt")
-        with open(output_test_preds_file, "w",encoding='UTF-8') as writer:
+        with open(output_test_preds_file, "w", encoding='UTF-8') as writer:
             writer.write("\n".join(test_preds))
